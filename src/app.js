e7c4f8fbac384627b0d13f6aabbafbe19f996e68
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-09-04 11:19:19 +1000

    Added in main.js entry point and application render view

---
+++ src/app.js
@@ -0,0 +1,11 @@
+export class App {
+    configureRouter(config, router) {
+        config.title = 'Aurelia Starter';
+
+        config.map([
+            // Define routes
+        ]);
+
+        this.router = router;
+    }
+}
