a7e1ede7ac661f3968f4baf17ec260441e169649
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-12-09 21:44:23 +1000

    chore(*): updating build process and configuration for listing

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,45 +1,17 @@
 var gulp = require('gulp');
 var bundler = require('aurelia-bundler');
+var bundles = require('../bundles.json');
 
 var config = {
   force: true,
   packagePath: '.',
-  bundles: {
-    "dist/app-build": {
-      includes: [
-        '*',
-        '*.html!text',
-        '*.css!text'
-      ],
-      options: {
-        inject: true,
-        minify: true
-      }
-    },
-    "dist/aurelia": {
-      includes: [
-        'aurelia-bootstrapper',
-        'aurelia-fetch-client',
-        'aurelia-router',
-        'github:aurelia/templating-binding',
-        'github:aurelia/templating-resources',
-        'github:aurelia/templating-router',
-        'github:aurelia/loader-default',
-        'github:aurelia/history-browser',
-        'github:aurelia/logging-console'
-      ],
-      options: {
-        inject: true,
-        minify: true
-      }
-    }
-  }
+  bundles: bundles.bundles
 };
 
 gulp.task('bundle', function() {
- return bundler.bundle(config);
+  return bundler.bundle(config);
 });
 
 gulp.task('unbundle', function() {
- return bundler.unbundle(config);
+  return bundler.unbundle(config);
 });
