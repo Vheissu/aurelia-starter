2110af1d9764dedf3188bb3397059cb95f5563ca
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-12-09 21:29:12 +1000

    feat(build): added in support for compiling Sass as well as standard styles

--- build/paths.js
+++ build/paths.js
@@ -7,9 +7,13 @@ module.exports = {
   root: appRoot,
   source: appRoot + '**/*.js',
   html: appRoot + '**/*.html',
-  css: appRoot + '**/*.css',
-  style: 'styles/**/*.css',
+  font: appRoot + 'assets/fonts/**/*',
+  style: [appRoot + 'assets/styles/**/*.scss', appRoot + 'assets/styles/**/*.css'],
+  image: appRoot + 'assets/images/**/*',
   output: outputRoot,
+  fontOutput: outputRoot + 'assets/fonts',
+  styleOutput: outputRoot + 'assets/styles',
+  imageOutput: outputRoot + 'assets/images',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/'
 };
--- build/tasks/build.js
+++ build/tasks/build.js
@@ -8,12 +8,10 @@ var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
 var notify = require("gulp-notify");
+
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// transpiles changed es6 files to SystemJS format
-// the plumber() call prevents 'pipe breaking' caused
-// by errors from other gulp plugins
-// https://www.npmjs.com/package/gulp-plumber
 gulp.task('build-system', function () {
   return gulp.src(paths.source)
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
@@ -24,42 +22,38 @@ gulp.task('build-system', function () {
     .pipe(gulp.dest(paths.output));
 });
 
-// copies changed html files to the output directory
 gulp.task('build-html', function () {
   return gulp.src(paths.html)
     .pipe(changed(paths.output, {extension: '.html'}))
     .pipe(gulp.dest(paths.output));
 });
 
-// copies changed css files to the output directory
-// copies changed css files to the output directory
-gulp.task('build-css', function () {
-  return gulp.src(paths.css)
-  .pipe(changed(paths.output, {extension: '.css'}))
-  .pipe(autoprefixer({
-    browsers: ['> 5%', 'Explorer >= 10']
-    }))
-    .pipe(gulp.dest(paths.output));
+gulp.task('build-styles', function() {
+    return gulp.src(paths.style)
+    .pipe(plumber())
+        .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(autoprefixer({
+                browsers: ['> 5%', 'Explorer >= 10']
+            }))
+        .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styleOutput));
 });
 
-// Build styles in the /styles directory
-gulp.task('build-styles', function () {
-  return gulp.src(paths.style)
-  .pipe(changed(paths.output, {extension: '.css'}))
-  .pipe(autoprefixer({
-    browsers: ['> 5%', 'Explorer >= 10']
-    }))
-  .pipe(gulp.dest(paths.output));
+gulp.task('build-images', function() {
+    return gulp.src(paths.image)
+        .pipe(gulp.dest(paths.imageOutput));
 });
 
-// this task calls the clean task (located
-// in ./clean.js), then runs the build-system
-// and build-html tasks in parallel
-// https://www.npmjs.com/package/gulp-run-sequence
+gulp.task('build-fonts', function() {
+    return gulp.src(paths.font)
+        .pipe(gulp.dest(paths.fontOutput));
+});
+
 gulp.task('build', function(callback) {
-  return runSequence(
-    'clean',
-    ['build-system', 'build-html', 'build-css', 'build-styles'],
-    callback
-  );
+    return runSequence(
+        'clean',
+        ['build-system', 'build-html', 'build-styles', 'build-images', 'build-fonts'],
+        callback
+    );
 });
