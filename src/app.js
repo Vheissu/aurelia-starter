4dec6f36d649ea5661d87474fce9532453e9774c
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-02-03 22:41:23 +1000

    feat(app): added in auth logic for checking routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
+import {Container} from 'aurelia-framework';
+
+import {Auth} from './common/auth/auth';
+
 export class App {
     configureRouter(config, router) {
         config.title = 'Aurelia Starter';
 
+        config.addPipelineStep('authorize', AuthorizeStep);
+
         config.map([
-            // Define routes
+            { route: ['home'], moduleId: 'home', title: 'Home', auth: true } }
         ]);
 
         this.router = router;
     }
+}
+
+
+class AuthorizeStep {
+  run(navigationInstruction, next) {
+    if (navigationInstruction.getAllInstructions().some(i => i.config.auth)) {
+      let isLoggedIn = Container.instance.get(Auth).isLoggedIn();
+
+      if (!isLoggedIn) {
+          return next.cancel(new Redirect('home'));
+      }
+    }
+
+    return next();
+  }
 }
